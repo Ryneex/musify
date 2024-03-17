d0fd84f6148dd43d023fa426b7e98761f2847fd3
MI Sajid <misajidsa@gmail.com>
2024-03-17 10:40:08 +0600

    feat: add audio player

---
+++ src/app/(routes)/(dashboard)/(home)/SongsSlider.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Song from '@/types/song.types'
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import player from '@/store/player.store'
+import { FaPlay } from 'react-icons/fa'
+
+type Props = {
+    songs: Song[]
+}
+
+export default function SongsSlider({ songs }: Props) {
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        align: 'start',
+        slidesToScroll: 3,
+        watchDrag: false,
+    })
+
+    return (
+        <div className="overflow-hidden">
+            <div className="flex items-center justify-between">
+                <h2 className="mb-4 text-xl font-bold text-black/95 dark:text-white/85">Trending Songs</h2>
+                <div className="flex select-none gap-2 text-2xl text-black/60 dark:text-white/60">
+                    <MdOutlineKeyboardArrowLeft className="cursor-pointer" onClick={() => emblaApi?.scrollPrev()} />
+                    <MdOutlineKeyboardArrowRight className="cursor-pointer" onClick={() => emblaApi?.scrollNext()} />
+                </div>
+            </div>
+            <div ref={emblaRef}>
+                <div className="flex gap-4">
+                    {songs.map((e) => (
+                        <div
+                            key={e.id}
+                            className="group relative flex shrink-0 basis-[13%] cursor-pointer flex-col gap-1 overflow-hidden rounded-2xl border border-black/10 bg-black/[0.01] p-3 backdrop-blur-lg transition duration-300 hover:bg-black/[0.08] dark:border-white/10 dark:bg-white/[0.05] dark:hover:bg-white/[0.08]"
+                        >
+                            <div className="relative w-full select-none overflow-hidden rounded-xl">
+                                <img className="aspect-square w-full object-cover" src={e.image} alt={e.song} />
+                                <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black/70 opacity-0 transition duration-300 group-hover:opacity-100">
+                                    <FaPlay
+                                        onClick={() => {
+                                            player.currentSong = e
+                                            player.SongList = songs
+                                        }}
+                                        className="text-3xl text-blue-400 transition duration-300 hover:scale-125"
+                                    />
+                                </div>
+                            </div>
+                            <span className="truncate px-1 pt-3 text-sm text-black/90 dark:text-white/90">
+                                {e.song}
+                            </span>
+                            <span className="truncate pl-1 text-xs text-black/70 dark:text-white/70">
+                                By {e.primary_artists}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/home/player/Player.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { Slider } from '@/components/home/player/slider'
+import { IoPlayOutline } from 'react-icons/io5'
+import { MdSkipNext, MdSkipPrevious } from 'react-icons/md'
+import { IoIosPause } from 'react-icons/io'
+import { Duration } from 'luxon'
+import { useSnapshot } from 'valtio'
+import player from '@/store/player.store'
+
+export default function Player() {
+    const audioRef = useRef(null as unknown as HTMLAudioElement)
+    const audio = audioRef.current
+    const [sliderValue, setSliderValue] = useState(0)
+    const [isPointerDown, setIsPointerDown] = useState(false)
+    const [currentTime, setCurrentTime] = useState(0)
+    const { currentSong, Playing } = useSnapshot(player)
+
+    // Formats song Duration
+    const duration = useMemo(() => {
+        const time = Duration.fromObject({ second: Number(currentSong.duration) || 0 }).toFormat('mm:ss')
+        return time
+    }, [currentSong])
+
+    // Formats currentTime
+    const formattedCurrentTime = useMemo(() => {
+        const time = Duration.fromObject({ second: currentTime }).toFormat('mm:ss')
+        return time
+    }, [currentTime])
+
+    // Handles Playback
+    useEffect(() => {
+        if (!audio) return
+        Playing && isPointerDown === false ? audio.play() : audio.pause()
+    }, [Playing, audio, isPointerDown, currentSong])
+
+    // Resets currentTime , SliderValue and plays the song when currentSong changes
+    useEffect(() => {
+        if (!currentSong.download_url) return
+        setSliderValue(0)
+        setCurrentTime(0)
+        player.Playing = true
+    }, [currentSong])
+
+    // Syncs Slider position based on songs currentTime
+    function updateSliderValue() {
+        if (isPointerDown || !audio?.duration) return
+        setSliderValue((audio.currentTime / audio.duration) * 100)
+        setCurrentTime(audio.currentTime)
+    }
+
+    // Updates songs currentTime when slider value changes
+    function updateCurrentTime([e]: any) {
+        if (!audio?.duration) return
+        setSliderValue(e)
+        audio.currentTime = (e / 100) * audio.duration
+        setCurrentTime(audio.currentTime)
+    }
+
+    return (
+        <div className="col-span-2 flex  select-none items-center justify-center gap-2 border-t-2 border-black/10 dark:border-white/10">
+            <audio onTimeUpdate={updateSliderValue} ref={audioRef} src={currentSong.download_url}></audio>
+            <div className="flex max-w-3xl basis-full gap-6">
+                <div className="flex items-center gap-3 text-2xl text-black/90 dark:text-white/90">
+                    <MdSkipPrevious className="cursor-pointer" />
+                    <div
+                        className="flex aspect-square w-6 cursor-pointer items-center justify-center overflow-hidden rounded-full"
+                        onClick={() => audio?.src && player.togglePlay()}
+                    >
+                        {Playing ? (
+                            <IoIosPause className="text-[20px]" />
+                        ) : (
+                            <IoPlayOutline className="ml-1 text-[20px]" />
+                        )}
+                    </div>
+                    <MdSkipNext className="cursor-pointer" />
+                </div>
+                <div className="flex basis-full items-center gap-3">
+                    <span className="text-xs text-black/90 dark:text-white/50">{formattedCurrentTime}</span>
+                    <Slider
+                        onPointerDown={(e) => e.button === 0 && setIsPointerDown(true)}
+                        onPointerUp={(e) => e.button === 0 && setIsPointerDown(false)}
+                        onValueChange={updateCurrentTime}
+                        value={[sliderValue]}
+                        max={100}
+                        step={0.1}
+                    />
+                    <span className="text-xs text-black/90 dark:text-white/50">{duration}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
